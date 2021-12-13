002a9c66a6443b58cd6ab12021e53dc9bb824bcc
Lu Yang <74538732+Luyabs@users.noreply.github.com>
2021-12-13 20:23:08 +0800

    Update Test.cpp
    一部分Test

--- 静态链表/Test.cpp
+++ 静态链表/Test.cpp
@@ -6,24 +6,41 @@ using namespace std;
 
 int main()
 {
-    SLink<double> a;
-    a.Insert(1.01); 
-    a.Insert(2.02);
-    a.Insert(3.03);
-    a.Traverse(true);
+	SLink<double> link;
+	char c = '$';
+	double e = 0;
 
-    a.Insert(2, 0.95);
-    a.Sort();
-    a.Traverse(true);
-
-    a.Reverse();
-    a.Traverse(true);
-
-    a.Delete(3);
-    a.Delete(1);
-    a.Delete(1);
-    a.Delete(1);
-    a.Traverse(true);
-    
-    //cout << a.Find(1.01) << endl;
+	while (c != 0)
+	{
+		cout << endl << "1. 生成单链表.";
+		cout << endl << "2. 显示单链表.";
+		cout << endl << "3. 取指定位置的元素.";
+		cout << endl << "0. 退出";
+		cout << endl << "选择功能(0~9,a~b):";
+		cin >> c;
+		switch (c)
+		{
+		case '1':
+			link.Reset();
+			cout << endl << "输入e(e = 666时退出):";
+			cin >> e;
+			while (e != 0) {
+				link.Insert(e);
+				cin >> e;
+			}
+			break;
+		case '2':
+			link.Traverse(true, cout);
+			break;
+		case '3':
+			cout << endl << "输入元素e:";
+			cin >> e;
+			e = link.Find(e);
+			if (link.Find(e) == -1)
+				cout << "元素不存在." << endl;
+			else
+				cout << "元素的位置:" << e << endl;
+			break;
+		}
+	}
 }
